6c079aad016bb543e0e2afc0e62f4ea183cf9f26
Hudson-obai <Hudsonobai@gmail.com>
2022-11-07 22:22:56 +0100

    Added project skeleton

--- README.md
+++
@@ -1,2 +0,0 @@
-# Team-Rio-Springboot
-Team Rio Backend written in Java's Springboot Framework for the SpaceYaTech Project
---
+++ src/test/java/com/spaceyatech/rio/RioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spaceyatech.rio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
